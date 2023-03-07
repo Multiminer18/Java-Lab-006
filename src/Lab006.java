73570aa7b4d8a44165f46e6f0dd1e8030b28870c
Multiminer18 <mccallevan18@gmail.com>
2023-03-07 12:12:44 -0800

    done!

---
+++ src/Lab006.java
@@ -0,0 +1,15 @@
+public class Lab006 {
+    //instance variable is only accessable with in the class Lab006
+
+    int n = 0;
+    while (n % 2 == 0)  {
+        System.out.println(n);
+        n++;
+    }
+    int m = 0;
+    while (m % 2 == 0) {
+        System.out.println(m);
+        m++;
+    }
+}
+
